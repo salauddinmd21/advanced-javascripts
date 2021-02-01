0dcca1bdea1e8b11d699b0fde2b02891adba65dc
salauddinmd21 <salauddinbusiness86@gmail.com>
2021-02-01 23:58:51 +0600

    true-false value of javascripts

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// false list:
+0
+""
+false
+NaN
+undefined
+null
+// truth list:
+"0", " ", {}, [],
+
+
+const name = "kalim uddin"
+if (name.length >0 ){
+    console.log('statement is true');
+}
+else{
+    console.log('statement is false');
+}
