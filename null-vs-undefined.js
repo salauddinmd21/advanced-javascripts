0e316b0b686501f21a082e3efd778b5dfc62b85b
salauddinmd21 <salauddinbusiness86@gmail.com>
2021-02-02 00:31:30 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// let pakhi;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);  
+// }
+
+//  const premik = {name: "smart dude", phone:' 01735919032'}
+// console.log(premik.gf);
+
+// const year = undefined;
+// console.log(year);
+
+// let ages = [12, 133,32];
+// console.log(ages[11]);
